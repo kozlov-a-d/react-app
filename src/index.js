ff213d85ad09c7eb70c0eb868890d12442eab9b3
Andrey Kozlov <andreydmitrievich666@gmail.com>
2018-03-12 22:13:43 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './views/app.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker(); 
